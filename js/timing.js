6331e2aad12e5de9f6978186f16cd6e7aba3449b
Valentin Gorbunov <valentin.gorbunov.17@ucl.ac.uk>
2020-03-16 03:05:21 +0000

    Built on navigation subsystem

--- js/timing.js
+++ js/timing.js
@@ -1,3 +1,9 @@
 function leftGeoFence(areaThatWasLeft) {
-	
+	timeTrackingEnabled = false;
+}
+
+function enteredGeoFence(areaThatWasEntered) {
+	timeTrackingEnabled = true;
+	document.getElementById('TimeTrack_Position_Text').innerHTML = areaThatWasEntered;
+	document.getElementById('nevigation_Destination_Name').innerHTML = areaThatWasEntered;
 }
