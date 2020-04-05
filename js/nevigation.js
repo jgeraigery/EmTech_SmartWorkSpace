159711c218dfc7cf415bf3c244e573cf57098c68
Valentin Gorbunov <valentin.gorbunov.17@ucl.ac.uk>
2020-04-05 13:07:42 +0100

    Removed redundant function definition

--- js/nevigation.js
+++ js/nevigation.js
@@ -123,20 +123,3 @@ function onSuccess(result) {
 	document.getElementById('nevigation_Destination_Head').innerHTML = "Location";
 	document.getElementById('nevigation_Destination_Name').innerHTML = "Personally Identifiable Information was removed";
 };
-
-function isMarkerInsidePolygon(coords, polyPoints) {       
-    var x = coords[0], y = coords[1];
-
-    var inside = false;
-    for (var i = 0, j = polyPoints.length - 1; i < polyPoints.length; j = i++) {
-        var xi = polyPoints[i][0], yi = polyPoints[i][1];
-        var xj = polyPoints[j][0], yj = polyPoints[j][1];
-
-        var intersect = ((yi > y) != (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
-        if (intersect) {
-        	inside = !inside;
-        }
-    }
-
-    return inside;
-};
